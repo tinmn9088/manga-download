ac5a63292ac6c4c1a9406d5a163e7a64df75291b
tinmn9088 <tinman.8.9@yandex.ru>
2022-09-09 01:00:51 +0300

    feat: add alert which tells how many volumes found

--- src/popup.tsx
+++ src/popup.tsx
@@ -6,12 +6,25 @@ function IndexPopup() {
   function handleOnClick(event: any) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, "generate", function(response) {
-        let result: string[] = Generator.generate(response);
-        let blob: Blob = new Blob([result.join("\n")], {type: "text/plain"});
-        let tempLink: HTMLAnchorElement = document.createElement("a");
-        tempLink.setAttribute("href", URL.createObjectURL(blob));
-        tempLink.setAttribute("download", "manga-download.sh");
-        tempLink.click();
+        let warning: HTMLElement = document.getElementById("warning");
+        warning.classList.remove("d-none");
+
+        if (!response || response.length === 0) {
+          warning.classList.add("alert-danger");
+          warning.innerText = "No volumes found";
+          return;
+        } else {
+          warning.classList.add("alert-success");
+          warning.innerText = `Volumes found: ${response.length}`;
+          setTimeout(() => {
+            let result: string[] = Generator.generate(response);
+            let blob: Blob = new Blob([result.join("\n")], {type: "text/plain"});
+            let tempLink: HTMLAnchorElement = document.createElement("a");
+            tempLink.setAttribute("href", URL.createObjectURL(blob));
+            tempLink.setAttribute("download", "manga-download.sh");
+            tempLink.click();
+          }, 2000);
+        }        
       });
     });
   };
@@ -25,14 +38,15 @@ function IndexPopup() {
         </label>
         <small id="authorHelp" className="form-text text-muted">Used to generate file names.</small>
       </div>
-      <div className="form-group row">
+      <div className="form-group row mb-4">
         <label>
           Title
           <input type="text" className="form-control" aria-describedby="titleHelp" placeholder="Enter title"></input>
         </label>
         <small id="titleHelp" className="form-text text-muted">Used to generate file names.</small>
       </div>
-      <button type="button" className="btn btn-dark mt-3" onClick={handleOnClick}>Generate</button>
+      <div className="alert d-none" role="alert" id="warning">Volumes found: </div>
+      <button type="button" className="btn btn-dark" onClick={handleOnClick}>Generate</button>
     </form>
   );
 }
