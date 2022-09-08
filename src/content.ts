b4ffa0b1018c20af2a00f0bcfb4aa9e002739fe8
tinmn9088 <tinman.8.9@yandex.ru>
2022-09-09 01:30:50 +0300

    feat: processor is chosen dynamically

--- src/content.ts
+++ src/content.ts
@@ -1,20 +1,18 @@
 import type { PlasmoContentScript } from "plasmo"
-import { MangabookProcessor } from "~src/mangabook-processor";
 import type { Volume } from "~src/models/volume";
 import type { Processor } from "~src/processor";
-import { MangafreakProcessor } from "./mangafreak-processor";
+import { ProcessorProvider } from "./processor-provider";
 
 export const config: PlasmoContentScript = {
   matches: ["https://mangabook.org/*", "https://w13.mangafreak.net/*"]
 };
 
-let processor: Processor = new MangafreakProcessor(15);
-
 window.addEventListener("load", () => {
-  console.log("Manga download content script loaded.");
-
+  console.log("Manga download content script loaded.", location.href);
+  
   chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
+      let processor: Processor = ProcessorProvider.provide();
       let volumes: Volume[] = processor.process();
       console.log("Volumes", volumes);
       sendResponse(volumes);
---
+++ src/processor-provider.ts
@@ -0,0 +1,21 @@
+import { MangabookProcessor } from "./mangabook-processor";
+import { MangafreakProcessor } from "./mangafreak-processor";
+import type { Processor } from "./processor";
+
+export class ProcessorProvider {
+
+  /**
+   * Processor depends 
+   */
+  static provide(): Processor {
+    let href: string = location.href;
+    switch (true) {
+      case /^https:\/\/mangabook\.org.*/.test(href):
+        return new MangabookProcessor();
+      case /^https:\/\/w13\.mangafreak\.net.*/.test(href):
+        return new MangafreakProcessor(15);
+      default:
+        throw new Error(`No providers for: ${href}`);
+    }
+  }
+}
